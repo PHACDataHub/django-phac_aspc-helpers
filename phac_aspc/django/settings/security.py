c5f07efb554181f9cb81716f8861b61901afca56
Luc Belliveau <luc.belliveau@phac-aspc.gc.ca>
2023-01-10 16:40:59 +0000

    feat: AC-7: Implemented account lockout controls

--- phac_aspc/django/settings/security.py
+++ phac_aspc/django/settings/security.py
@@ -1,5 +1,29 @@
 """Recommended values related to security controls"""
-from .utils import global_from_env
+from .utils import global_from_env, configure_authentication_backends
+
+#  AC-7 Automatic lockout of users after invalid login attempts
+AUTHENTICATION_BACKENDS = configure_authentication_backends([
+    'django.contrib.auth.backends.ModelBackend',
+])
+
+# Lockout users based on their username and IP address
+AXES_LOCK_OUT_BY_COMBINATION_USER_AND_IP = True
+
+# Log unsuccessful logins
+AXES_ENABLE_ACCESS_FAILURE_LOG = True
+
+# After 3 failed login attempts, lock out the combination (IP + user).
+AXES_FAILURE_LIMIT = 3
+
+# After 30 minutes, locked out accounts are automatically unlocked.
+AXES_COOLOFF_TIME = 0.5
+
+# Reverse proxy configuration
+AXES_PROXY_COUNT = 1  # (Behind 1 proxy)
+AXES_META_PRECEDENCE_ORDER = [
+    'HTTP_X_FORWARDED_FOR',
+    'REMOTE_ADDR',
+]
 
 #  AC-11 - Session controls
 global_from_env(
--- phac_aspc/django/settings/utils.py
+++ phac_aspc/django/settings/utils.py
@@ -3,6 +3,67 @@ import inspect
 
 import environ
 
+from django.core import checks
+
+
+def trigger_configuration_warning(msg, **kwargs):
+    """Trigger a configuration error programmatically"""
+
+    def phac_aspc_conf_warning(app_configs, **conf_kwargs):  # pylint: disable=unused-argument
+        return [checks.Warning(msg, **kwargs)]
+
+    checks.register(phac_aspc_conf_warning)
+
+
+def warn_and_remove(items, dest_list):
+    """If the items intersects with dest_list, raise a configuration warning and
+    remove it from the list returned to prevent errors."""
+    ret = []
+    context = inspect.stack()[1].function
+    hint = "You should remove it from your project to ensure proper ordering."
+
+    for item in items:
+        if item in dest_list:
+            trigger_configuration_warning(
+                f"{item} is already defined in the provided list.",
+                id=context,
+                hint=hint,
+            )
+            hint = None
+        else:
+            ret.append(item)
+    return ret
+
+
+def configure_apps(app_list):
+    """Return an application list which includes the apps required by this
+    library"""
+
+    prefix_list = warn_and_remove(["modeltranslation", "axes"], app_list)
+    suffix_list = warn_and_remove(["phac_aspc.django.helpers"], app_list)
+
+    return prefix_list + app_list + suffix_list
+
+
+def configure_authentication_backends(backend_list):
+    """Return the authentication backend list includes those required by this
+    library.
+
+    By default importing the settings will automatically configure the backend,
+    however if you want to customize the authentication backend used by your
+    project, you can use this method to ensure proper configuration."""
+
+    prefix_backends = warn_and_remove(
+        ["axes.backends.AxesStandaloneBackend"], backend_list
+    )
+    return prefix_backends + backend_list
+
+
+def configure_middleware(middleware_list):
+    """Return the list of middleware configured for this library"""
+    suffix = warn_and_remove(["axes.middleware.AxesMiddleware"], middleware_list)
+    return suffix + middleware_list
+
 
 def global_from_env(prefix="PHAC_ASPC_", **conf):
     """Create named global variables based on the provided environment variable
