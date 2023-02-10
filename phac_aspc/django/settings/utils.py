5f72bcae954ead10f5acf4f6156f3f82b7996f33
Luc Belliveau <luc.belliveau@phac-aspc.gc.ca>
2023-02-10 03:28:05 +0000

    fix: Added utility to alter settings during unit tests

--- phac_aspc/django/settings/utils.py
+++ phac_aspc/django/settings/utils.py
@@ -4,6 +4,7 @@ import inspect
 import environ
 
 from django.core import checks
+from django.conf import settings
 
 
 def trigger_configuration_warning(msg, **kwargs):
@@ -93,3 +94,18 @@ def global_from_env(prefix="PHAC_ASPC_", **conf):
 
     for name in conf:
         setattr(mod, name, env(f"{prefix}{name}"))
+
+
+def configure_settings_for_tests():
+    """
+    Modify django settings so they are suitable to run unit tests.
+      - Disables the Axes authentcation backend, to allow the test client
+        to function.  You could instead modify the request as described here:
+        https://django-axes.readthedocs.io/en/latest/3_usage.html#authenticating-users
+    """
+    # settings.AXES_ENABLED = False
+    settings.AUTHENTICATION_BACKENDS = [
+        x for x in
+            settings.AUTHENTICATION_BACKENDS
+            if x != 'axes.backends.AxesStandaloneBackend'
+    ]
