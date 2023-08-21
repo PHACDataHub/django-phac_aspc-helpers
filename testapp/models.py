309f5148911562d2cc90a4eba8ac89814f85e524
AlexCLeduc <ledualex@gmail.com>
2023-08-21 14:20:25 -0400

    use custom fields in testapp models

--- testapp/models.py
+++ testapp/models.py
@@ -2,6 +2,8 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
+from phac_aspc.django import fields
+
 from django.contrib.auth.models import AbstractUser
 
 
@@ -10,12 +12,12 @@ class User(AbstractUser):
 
 
 class Author(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
+    first_name = fields.CharField(max_length=100)
+    last_name = fields.CharField(max_length=100)
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=250)
+    name = fields.CharField(max_length=250)
 
     def __str__(self):
         return self.name
@@ -24,6 +26,6 @@ class Tag(models.Model):
 class Book(models.Model):
     # changelog_live_name_loader_class = BookNameLoader
 
-    author = models.ForeignKey(Author, related_name="books", on_delete=models.CASCADE)
-    title = models.CharField(max_length=250)
-    tags = models.ManyToManyField(Tag)
+    author = fields.ForeignKey(Author, related_name="books", on_delete=models.CASCADE)
+    title = fields.CharField(max_length=250)
+    tags = fields.ManyToManyField(Tag)
