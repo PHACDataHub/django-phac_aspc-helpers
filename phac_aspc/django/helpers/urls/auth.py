a07722dba1c475b29d0fe9e2379b78c78c9e89ba
Keenan <keenandhanani@gmail.com>
2025-09-23 08:32:46 -0400

    cleanup

--- phac_aspc/django/helpers/urls/auth.py
+++ phac_aspc/django/helpers/urls/auth.py
@@ -1,10 +1,10 @@
 """Setup URLs for views related to authentication"""
+
 from django.urls import path
 from django.conf.urls.i18n import i18n_patterns
 
 from phac_aspc.django.helpers.auth.views import login, authorize
 from phac_aspc.django.settings.security_env import get_oauth_env_value
-print("provider", get_oauth_env_value("PROVIDER"))
 
 urlpatterns = (
     i18n_patterns(
